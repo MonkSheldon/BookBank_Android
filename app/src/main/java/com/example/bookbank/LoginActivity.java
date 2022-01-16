bd72fb015f4273377f7a75ff2fc02d300999df6f
Lorenzo Pagnoni <S4477837@studenti.unige.it>
2022-01-16 09:47:38 +0100

    Sviluppo della richiesta di login email/password e creazione dell'activity catalog. Manca controllare la sessione su login.

--- app/src/main/java/com/example/bookbank/LoginActivity.java
+++ app/src/main/java/com/example/bookbank/LoginActivity.java
@@ -1,23 +1,39 @@
 package com.example.bookbank;
 
+import static misc.Constants.path_backend;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class LoginActivity extends Activity {
 
+    private final String TAG = "Login Activity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //TODO control session
 
         TextInputLayout tilemail = findViewById(R.id.til_email);
         TextView txtforgotpwd = findViewById(R.id.txt_forgot_pwd);
@@ -45,7 +61,42 @@ public class LoginActivity extends Activity {
                 }
             }
             else {
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("email", email);
+                    jsonObject.put("pwd", pwd);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
 
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                        Request.Method.POST,
+                        path_backend + "/api/login",
+                        jsonObject,
+                        response -> {
+                            try {
+                                if (response.getBoolean("success")) {
+                                    Log.i(TAG, "Login email/pwd ok");
+                                    SharedPreferences pref = LoginActivity.this.getSharedPreferences("Session Data", MODE_PRIVATE);
+                                    SharedPreferences.Editor edit = pref.edit();
+                                    edit.putString("email", email);
+                                    edit.apply();
+                                    Intent i = new Intent(getString(R.string.launch_catalog));
+                                    startActivity(i);
+                                }
+                                else {
+                                    Log.i(TAG, "Login email/pwd failed");
+                                    Toast.makeText(LoginActivity.this, "Email and/or password are wrong", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        },
+                        error -> Log.i(TAG, error.toString())
+                );
+
+                requestQueue.add(jsonObjectRequest);
             }
         });
 
---
+++ app/src/main/java/misc/Constants.java
@@ -0,0 +1,6 @@
+package misc;
+
+public class Constants {
+
+    public static final String path_backend = "https://russelpopi.synology.me";
+}
