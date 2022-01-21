f5aadaebf95bd175ba2560ce52349722be8c2fc5
Lorenzo Pagnoni <S4477837@studenti.unige.it>
2022-01-21 17:11:40 +0100

    Sviluppo getbooks ed aggiunto la TextView per il messaggio "Nessun libro".

--- app/src/main/java/com/example/bookbank/CatalogFragment.java
+++ app/src/main/java/com/example/bookbank/CatalogFragment.java
@@ -1,17 +1,36 @@
 package com.example.bookbank;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import static misc.Constants.path_backend;
+
 import androidx.fragment.app.Fragment;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class CatalogFragment extends Fragment {
+
+    private final String TAG = "Catalog Fragment";
 
     private final boolean favorite;
 
@@ -25,18 +44,50 @@ public class CatalogFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_catalog, container, false);
         ListView lvbooks = view.findViewById(R.id.lv_books);
         ArrayList<Book> booklist = new ArrayList<>();
+        TextView tvnobooks = view.findViewById(R.id.tv_no_book);
 
-        //TODO getbooks
+        SharedPreferences pref = view.getContext().getSharedPreferences("Session Data", MODE_PRIVATE);
 
-        Book book = new Book("Book0", "Alessio", "20 $", true, "l dep");
-        booklist.add(book);
-        for (int i=0; i<20; ++i) {
-            book = new Book("Book" + i, "Alessio", "20 $", false, "l dep");
-            booklist.add(book);
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", pref.getString("email", null));
+            jsonObject.put("favorite", favorite);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        RequestQueue requestQueue = Volley.newRequestQueue(view.getContext());
 
-        BookListAdapter adapter = new BookListAdapter(view.getContext(), R.layout.adapter_view_book, booklist);
-        lvbooks.setAdapter(adapter);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                path_backend + "/api/getbooks",
+                jsonObject,
+                response -> {
+                    try {
+                        JSONArray books = response.getJSONArray("books");
+                        if (books.length() == 0) {
+                            tvnobooks.setVisibility(View.VISIBLE);
+                        }
+                        else {
+                            for (int i=0; i<books.length(); ++i) {
+                                JSONObject jsonbook = books.getJSONObject(i);
+                                booklist.add(new Book(jsonbook.getString("name"),
+                                        jsonbook.getString("author"),
+                                        jsonbook.getString("price"),
+                                        jsonbook.getBoolean("favorite"),
+                                        jsonbook.getString("tags")));
+                            }
+
+                            BookListAdapter adapter = new BookListAdapter(view.getContext(), R.layout.adapter_view_book, booklist);
+                            lvbooks.setAdapter(adapter);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Log.i(TAG, error.toString())
+        );
+
+        requestQueue.add(jsonObjectRequest);
 
         return view;
     }
